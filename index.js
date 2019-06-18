b4e1d101681c0b5b05a2a96f17f746594cb6a992
Michael Schiumo <mschiumo18@gmail.com>
2019-06-18 16:02:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+
+
+function writeCards(["Lisa", "Kaitlin", "Jan"], "birthday") {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+  }
+    return writeCards(cards);
+}
