5e34a4a03d62aa750bb9422a9a07d42709736598
Michael Schiumo <mschiumo18@gmail.com>
2019-06-18 16:17:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 // Code your solutions in this file
 const namesArray = ["Lisa", "Kaitlin", "Jan"]
 
-function writeCards(namesArray, "birthday") {
+function writeCards(namesArray, birthday) {
   let cards = []
-  for (let i = 0; i < cards.length; i++) {
-    cards.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+  for (let i = 0; i < namesArray.length; i++) {
+    cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${birthday} gift!`);
   }
     return cards;
+}
+
+function countdown() {
+  let i = 0;
+  while (i > 0) {
+    console.log(i--)
+  }
 }
