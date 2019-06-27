4a48f6634e21d145d1f2204415652758f29d5f0d
Irfan Maknojia <imak_7@yahoo.com>
2019-06-26 21:26:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+import { eventNames } from "cluster";
+
 // Code your solutions in this file
+function writeCards(names, event) {
+    const cardMessages = []
+    for(let i = 0; i < names.length; i++){
+        cardMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cardMessages
+}
+
+function countdown(integer) {
+    while (integer > 0) {
+        console.log(integer)
+        integer -= 1
+    }
+    console.log(integer)
+}
