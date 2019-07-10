b673b2bb84fff68a49d2b474090547e1d6dce986
Dmitri Samotessov <dimasamo96@gmail.com>
2019-07-10 22:00:32 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, eventName){
+  let retArray=[]
+ for (let i = 0; i < array.length; i++) {
+   retArray.push(`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`)
+ }
+ return retArray
+}
+
+function countdown(num){
+  while (num>=0) {
+    console.log(num--)
+
+  }
+}
